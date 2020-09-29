e459471481c9f39077e552f5265210946c8dac77
LucaRiccio <lucariccio888@gmail.com>
2020-09-29 17:07:38 +0200

    1° Parte (Estrapolazione dati camera in tabella)

--- index-php.php
+++ index-php.php
@@ -1,13 +1,38 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="dist/app.css">
-    <title>php-crud-read-create-update</title>
-  </head>
+<?php
+
+include __DIR__ . '/partials/templates/header.php';
+include __DIR__ . '/partials/home/server.php';
+
+?>
+
   <body>
 
-  
+    <div class="container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Numero Stanza</th>
+            <th>Floor</th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($results as $room){?>
+          <tr>
+            <td><?php echo $room['id']; ?></td>
+            <td><?php echo $room['room_number']; ?></td>
+            <td><?php echo $room['floor']; ?></td>
+            <td><a href="#">VIEW</a></td>
+            <td><a href="#">UPDATE</a></td>
+            <td><a href="#">DELETE</a></td>
+          </tr>
+        <?php } ?>
+        </tbody>
+      </table>
+    </div>
 
   </body>
 </html>
---
+++ partials/env-example.php
@@ -0,0 +1,6 @@
+<!-- Modello da pushare -->
+<?php
+$servername = "localhost";
+$username = "usarname";
+$password = "password";
+$dbname = "dbname";
